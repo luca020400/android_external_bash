96e248dd68a082f94cbf06aab8d1b5d70f0dae4a
Michael Bestas <mkbestas@lineageos.org>
2017-12-23 18:32:14 +0200

    Update version.h after recent updates
    Change-Id: I2ca3c48ee135de4798d2ce107275a1689f6b12b0

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
    `make version.h' to the Makefile.  It is created by mkversion. */
 
 /* The distribution version number of this shell. */
-#define DISTVERSION "4.3"
+#define DISTVERSION "4.4"
 
 /* The last built version of this shell. */
 #define BUILDVERSION 1
@@ -11,7 +11,7 @@
 #define RELSTATUS "release"
 
 /* The default shell compatibility-level (the current version) */
-#define DEFAULT_COMPAT_LEVEL 43
+#define DEFAULT_COMPAT_LEVEL 44
 
 /* A version string for use by sccs and the what command. */
-#define SCCSVERSION "@(#)Bash version 4.3.28(1) release GNU"
+#define SCCSVERSION "@(#)Bash version 4.4.12(1) release GNU"
